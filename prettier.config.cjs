5313fe0a1a6562d221533c8fd29e0f5cc6ffd6ee
Abinash Panda <abinash@prodios.com>
2023-08-17 12:09:41 +0530

    feat(setup): add prettier and eslint

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Options} */
+const config = {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss']
+}
+
+module.exports = config
