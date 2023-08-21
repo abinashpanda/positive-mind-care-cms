709d2f158385e73fb26d9440c4f5870c60d26a74
Abinash Panda <abinash@prodios.com>
2023-08-21 18:50:06 +0530

    feat(next): disable eslint in build stage

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import './src/env.mjs'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default nextConfig
