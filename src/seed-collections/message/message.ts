693bdde9d86d493e15480e68ae8ccd2f0a29b545
ShubhamRavani <shubhamravani67890@gmail.com>
2023-08-22 17:31:32 +0530

    feat(positive-mind-care-cms): add seed script for message

---
+++ src/seed-collections/message/message.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/firestore'
+import * as messageData from './message.json'
+
+const messageCollectionRef = firebase.firestore().collection('message')
+
+async function seedCollection() {
+  for (const data of messageData) {
+    await messageCollectionRef.add(data)
+  }
+  console.log('Message added to the collection.')
+}
+
+seedCollection()
+  .then(() => {
+    console.log('Seed process completed.')
+  })
+  .catch((error) => {
+    console.error('Error seeding data:', error)
+  })
