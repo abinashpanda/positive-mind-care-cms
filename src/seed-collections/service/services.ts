527cf46cc586de11b63f611cbb8bf7c508671aad
ShubhamRavani <shubhamravani67890@gmail.com>
2023-08-24 02:32:53 +0530

    fix(positive-mind-care-cms): update seed script and json for services

---
+++ src/seed-collections/service/services.ts
@@ -0,0 +1,29 @@
+import admin from 'firebase-admin'
+import chalk from 'chalk'
+import serviceAccount from '../service-account.json'
+import serviceData from './service.json'
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+})
+const firestore = admin.firestore()
+
+async function main() {
+  const servicesCollectionRef = firestore.collection('services')
+  for (const service of serviceData) {
+    // eslint-disable-next-line no-console
+    console.log(chalk.blue(`🕐 Adding service:\t\t\t${service.name}`))
+    try {
+      await servicesCollectionRef.add(service)
+      // eslint-disable-next-line no-console
+      console.log(chalk.green(`✅ Successfully added service:\t${service.name}`))
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      // eslint-disable-next-line no-console
+      console.log(chalk.red(`❌ Error adding service:\t\t${service.name}`))
+    }
+  }
+}
+
+main()
